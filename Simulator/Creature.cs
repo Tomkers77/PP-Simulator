5c16bf2d9281857f18d69a54bd078bbb280da55d
Tomkers77 <t.nykiel23@gmail.com>
2024-12-10 23:22:56 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Simulator;
+
+internal class Creature
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+
+    //--------------------------------------------------
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public Creature() { }
+
+    //---------------------------------------------------
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+    }
+
+    public string Info
+    {
+        get { return $"{Name} [{Level}]"; }
+    }
+}
+
+   
