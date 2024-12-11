587939e68b55ec4ec078b9918c26df7a71a94994
Tomkers77 <t.nykiel23@gmail.com>
2024-12-11 22:00:06 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -3,19 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Simulator;
 
 public class Animals
 {
-    public required string Description { get; init; }
+    private string _description = "No Data";
+    public required string Description 
+    { 
+        get { return _description; }
+        init
+        {
+            string description = value.Trim();
+
+            if (description.Length > 15)
+            {
+                description = description.Remove(15).TrimEnd();
+            }
+
+            if (description.Length < 3)
+            {
+                description = description.PadRight(3, '#');
+            }
+
+            if (char.IsLower(description[0]))
+            {
+                description = char.ToUpper(description[0]) + description.Substring(1);
+            }
+
+            _description = description;
+        } 
+    }
     public uint Size { get; set; } = 3;
 
     public string Info
     {
         get
         {
-            return $"{Description}<{Size}>";
+            return $"{Description} <{Size}>";
         }
 
     }
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -11,8 +12,53 @@ namespace Simulator;
 
 internal class Creature
 {
-    public string Name { get; set; }
-    public int Level { get; set; }
+    private string _name = "Unknown";
+    public string Name
+    {
+        get { return _name; }
+        init
+        {
+            
+            string name = value.Trim();
+            
+            if (name.Length > 25)
+            {
+                name = name.Remove(25).TrimEnd();
+            }
+
+            if (name.Length < 3) 
+            {
+                name = name.PadRight(3, '#');
+            }
+
+            if (char.IsLower(name[0]))
+            {
+                name = char.ToUpper(name[0]) + name.Substring(1);
+            }
+            
+            _name = name;
+        }
+    }
+
+    private int _level = 1;
+    public int Level 
+    { 
+        get { return _level; }
+        init
+        {
+            int level = value;
+            if (level < 1)
+            {
+                level = 1;
+            }
+            if (level > 10)
+            {
+                level = 10;
+            }
+
+             _level = level;
+        } 
+    }
 
     //--------------------------------------------------
     public Creature(string name, int level = 1)
@@ -32,6 +78,14 @@ internal class Creature
     public string Info
     {
         get { return $"{Name} [{Level}]"; }
+    }
+
+    public void Upgrade() 
+    { 
+        if (Level < 10)
+        {
+            _level++;
+        }
     }
 }
 
