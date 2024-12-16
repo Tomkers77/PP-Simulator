102cb2fd2936ea004aa51e8ec7dfe9a9327f5369
Tomkers77 <t.nykiel23@gmail.com>
2024-12-16 22:41:34 +0100

    TDD

--- Simulator/Maps/SmallTorusMap.cs
+++ Simulator/Maps/SmallTorusMap.cs
@@ -5,32 +5,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Simulator.Maps
+namespace Simulator.Maps;
+
+public class SmallTorusMap : Map
 {
-    internal class SmallTorusMap : Map
+    public int Size {get; } 
+
+    public SmallTorusMap(int size)
     {
-        public int Size {get; } 
-
-        public SmallTorusMap(int size)
+        if (size >= 5 && size <= 20)
         {
-            throw new NotImplementedException();
+            Size = size;
         }
-
-        //----------------------------------------------------------------------
-
-        public override bool Exist(Point p)
+        else
         {
-            throw new NotImplementedException();
+            throw new ArgumentOutOfRangeException("Podano niewłaściwy rozmiar. Podaj liczbę z zakresu <5,20>");
         }
+    }
 
-        public override Point Next(Point p, Direction d)
-        {
-            throw new NotImplementedException();
-        }
+    //----------------------------------------------------------------------
 
-        public override Point NextDiagonal(Point p, Direction d)
-        {
-            throw new NotImplementedException();
-        }
+    public override bool Exist(Point p)
+    {
+        Rectangle exRec = new Rectangle(0, 0, Size - 1, Size - 1);
+        bool check = exRec.Contains(p);
+        return check;
+    }
+
+    public override Point Next(Point p, Direction d)
+    {
+        Point nextPoint = new Point();
+        nextPoint = p.Next(d);
+
+        int newNextPointX = (nextPoint.X + Size) % Size;
+        int newNextPointY = (nextPoint.Y + Size) % Size;
+
+        Point newNextPoint = new Point(newNextPointX, newNextPointY);
+        return newNextPoint;
+    }
+    
+
+    public override Point NextDiagonal(Point p, Direction d)
+    {
+        Point nextPoint = new Point();
+        nextPoint = p.NextDiagonal(d);
+
+        int newNextPointX = (nextPoint.X + Size) % Size;
+        int newNextPointY = (nextPoint.Y + Size) % Size;
+
+        Point newNextPoint = new Point(newNextPointX, newNextPointY);
+        return newNextPoint;
     }
 }
